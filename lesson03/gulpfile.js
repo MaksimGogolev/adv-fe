a9506b26aced6ed195df90671344f8da1d39ad38
Maksim Gogolev <mgglev@gmail.com>
2016-08-03 20:49:06 +0300

    lesson3 have been done except advanced tasks

--- lesson03/gulpfile.js
+++ lesson03/gulpfile.js
@@ -7,17 +7,17 @@ var less = require('gulp-less');
 var argv = require('yargs').argv;
 var debug = require( 'gulp-debug' );
 var clean = require( 'gulp-clean' );
-var livereload = require('gulp-livereload');
 var csscomb = require('gulp-csscomb');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
+var htmlhint = require('gulp-htmlhint');
 var runSequence = require('run-sequence');
 var sourcemaps = require('gulp-sourcemaps');
 var cssnano = require('gulp-cssnano');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
-
-gulp.task('default', ['libs', 'build']);
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 gulp.task('bower', function () {
     return bower();
@@ -39,9 +39,7 @@ gulp.task('images', function () {
 
 gulp.task('html', function () {
     return gulp.src(['client_src/**/*.html', '!client_src/libs/**/*.*'])
-        .pipe(gulpif(!argv.prod, sourcemaps.init()))
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulpif(!argv.prod, sourcemaps.write()))
+        .pipe(gulpif(argv.prod, htmlmin({collapseWhitespace: true})))
         .pipe(gulp.dest(destDir));
 });
 
@@ -50,6 +48,10 @@ gulp.task('css', function () {
         .pipe(gulpif(!argv.prod, sourcemaps.init()))
         .pipe(concat('styles.css'))
         .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(cssnano())
         .pipe(gulpif(!argv.prod, sourcemaps.write()))
         .pipe(gulp.dest(destDir + '/static'));
@@ -67,7 +69,7 @@ gulp.task('js', function () {
 gulp.task( 'clean', function () {
     return gulp.src( destDir + '/*', { read: false } )
         .pipe( clean( { force: true } ) );
-} );
+});
 
 
 gulp.task( 'watch', function () {
@@ -75,38 +77,55 @@ gulp.task( 'watch', function () {
     gulp.watch(['client_src/**/*.{png,jpg,svg}', '!client_src/libs/**/*.*'], [ 'images' ] );
     gulp.watch(['client_src/**/*.html', '!client_src/libs/**/*.*'], [ 'html' ] );
     gulp.watch(['client_src/**/*.js', '!client_src/libs/**/*.*'], [ 'js' ] );
-} );
+});
 
-gulp.task( 'reload-page', function () {
-} );
+gulp.task( 'serve', function () {
+    browserSync.init({
+        server: 'bin'
+    });
+    browserSync.watch('bin/**/*.*').on('change', browserSync.reload);
+});
 
-//
-// //CODESTYLE
-// gulp.task('csscomb', function () {
-//     return gulp.src('styles/*.less')
-//         .pipe(csscomb().on('error', handleError))
-//         .pipe(gulp.dest(function (file) {
-//             return file.base;
-//         }));
-// });
-//
-// gulp.task('htmlhint', function () {
-// });
-//
-// gulp.task('jscs', function () {
-// });
-//
-// gulp.task('jshint', function () {
-// });
-//
-// gulp.task('style', function () {
-// });
-//
-// //CODESTYLE//
-//
-// function handleError(err) {
-//     console.log(err.toString());
-//     this.emit('end');
-//     return this;
-// }
-//
+gulp.task('default', ['libs', 'build', 'watch','serve']);
+
+
+//CODESTYLE
+gulp.task('csscomb', function () {
+    return gulp.src(['client_src/**/*.less', '!client_src/libs/**/*.*'])
+        .pipe(csscomb().on('error', handleError))
+        .pipe(gulp.dest(function (file) {
+            return file.base;
+        }));
+});
+
+gulp.task('htmlhint', function () {
+    return gulp.src(['client_src/**/*.html', '!client_src/libs/**/*.*'])
+        .pipe(htmlhint('.htmlhintrc'))
+        .pipe(htmlhint.reporter());
+});
+
+gulp.task('jscs', function () {
+    return gulp.src(['client_src/**/*.js', '!client_src/libs/**/*.*'])
+        .pipe(jscs({fix: true}))
+        .pipe(jscs.reporter())
+        .pipe(gulp.dest('client_src'));
+});
+
+gulp.task('jshint', function () {
+    return gulp.src(['client_src/**/*.js', '!client_src/libs/**/*.*'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('style', function () {
+    runSequence('jshint', 'jscs', 'htmlhint', 'csscomb');
+});
+
+// CODESTYLE
+
+function handleError(err) {
+    console.log(err.toString());
+    this.emit('end');
+    return this;
+}
+
